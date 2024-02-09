19c66b398672238c53b935bd75bcbe7187b983d7
OpyssMagnuum <135262782+OpyssMagnuum@users.noreply.github.com>
2024-02-09 11:04:20 +0300

    Added SQL script

---
+++ Musical_Site2.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS Genre(
+	id SERIAL primary key,
+	name VARCHAR(60) not null
+);
+
+CREATE TABLE IF NOT EXISTS Artist(
+	id SERIAL primary key,
+	name VARCHAR(60) not null
+);
+
+CREATE TABLE IF NOT EXISTS Album(
+	id SERIAL primary key,
+	name VARCHAR(60) not null,
+	release_date VARCHAR(10) not null
+);
+
+CREATE TABLE IF NOT EXISTS Track(
+	id SERIAL primary key references Album(id),
+	name VARCHAR(60) not null,
+	duration VARCHAR(10) not null
+);
+
+CREATE TABLE IF NOT EXISTS Collection(
+	id SERIAL primary key,
+	name VARCHAR(60) not null,
+	release_date VARCHAR(10) not null
+);
+
+CREATE TABLE IF NOT exists genre_artist(
+	genre_id INTEGER references Genre(id),
+	artist_id INTEGER references Artist(id),
+	constraint ga primary key (genre_id, artist_id)	
+);
+
+CREATE TABLE IF NOT exists album_artist(
+	album_id INTEGER references Album(id),
+	artist_id INTEGER references Artist(id),
+	constraint aa primary key (album_id, artist_id)	
+);
+
+CREATE TABLE IF NOT exists album_collection(
+	album_id INTEGER references Album(id),
+	collection_id INTEGER references Collection(id),
+	constraint ac primary key (album_id, collection_id)	
+);
+
+CREATE TABLE IF NOT exists track_collection(
+	track_id INTEGER references Track(id),
+	collection_id INTEGER references Collection(id),
+	constraint tc primary key (track_id, collection_id)	
+);
